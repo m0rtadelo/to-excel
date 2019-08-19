821e8b0cca028b1ee0b2fecfb8ccaad9c7d0c2a9
Ricard Figuls <ricard.figuls@gmail.com>
2019-08-19 22:45:33 +0200

    ADD browser build

---
+++ travis/build.js
@@ -0,0 +1,12 @@
+const te = require('../index').ToExcel;
+const fs = require('fs');
+
+var data = "function toExcel() {}";
+data += "\ntoExcel.download = " + te.download.toString();
+data += "\ntoExcel.exportXLS = " + te.exportXLS.toString();
+data += "\ntoExcel.generateXML = " + te.generateXML.toString();
+data += "\ntoExcel.getData = " + te.getData.toString();
+data += "\ntoExcel.parseXML = " + te.parseXML.toString();
+
+fs.writeFileSync('../docs/to-excel.js', data);
+// console.log(data);
